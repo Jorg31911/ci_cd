442703a13008ab844e3442bd0d7b39b46c098811
Jorge <jorg31911@gmail.com>
2021-05-31 11:16:47 -0500

    second

--- site.py
+++ site.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def principal():
-    return "<h1> prueba 1 </h1><button>clear<button>"
+    return render_template('home.html')
+
+
+@app.route("/about")
+def principal():
+    return 'about'
 
 
 if __name__ == '__main__':
